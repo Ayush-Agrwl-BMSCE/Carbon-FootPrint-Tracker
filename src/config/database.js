bc4d0d7dda1fff16c4d763b13fcdb822e1fc8622
Ayush-Agrwl-BMSCE <ayushkumar.ci24@bmsce.ac.in>
2026-01-14 15:49:04 +0530

    Add files via upload

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+const getConnection = (callback) => {
+  pool.getConnection(callback);
+};
+
+module.exports = { getConnection, pool };
